5c98288317d1dc5a570fc724a7d3ab75bb396a95
Abhinav Jain <jainabhinav2412@gmail.com>
2025-01-18 14:08:56 +0530

    Remove bug due to non search google page

--- background.js
+++ background.js
@@ -1,22 +1,28 @@
 let injectedTabSearch = new Map();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const allowInjection =
-    changeInfo.url && // Ensure it's a URL change
-    new URL(changeInfo.url).hostname.includes("www.google.co") && // Ensure the changed url is of google
-    (!injectedTabSearch.has(tabId) ||
-      getSearchQuery(new URL(changeInfo.url)) !== injectedTabSearch.get(tabId)); // Ensure that either it's a new tab or change in search for the same tab
+  const url = changeInfo.url // Ensure it's a URL change
+    ? new URL(changeInfo.url)
+    : null;
+  if (url !== null) {
+    const searchQuery = getSearchQuery(url);
+    const allowInjection =
+      url.hostname.includes("www.google.co") && // Ensure the changed url is of google
+      url.pathname === "/search" &&
+      (!injectedTabSearch.has(tabId) ||
+        searchQuery !== injectedTabSearch.get(tabId)); // Ensure that either it's a new tab or change in search for the same tab
 
-  if (allowInjection) {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabId },
-        files: ["contentScript.js"],
-      },
-      () => chrome.runtime.lastError
-    );
-    if (new URL(changeInfo.url).search) {
-      injectedTabSearch.set(tabId, getSearchQuery(new URL(changeInfo.url)));
+    if (allowInjection) {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabId },
+          files: ["contentScript.js"],
+        },
+        () => chrome.runtime.lastError
+      );
+      if (url.search) {
+        injectedTabSearch.set(tabId, searchQuery);
+      }
     }
   }
 });
